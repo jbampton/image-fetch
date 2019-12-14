3e799376f0614c4f8e749b359376e48edf0f590a
spectralapps <56676505+spectralapps@users.noreply.github.com>
2019-12-14 04:53:38 -0300

    Updates

---
+++ precache-manifest.a0fe416305bcb5e461c47538fa881fe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaba342206b58363da2adadb53b778ff",
+    "url": "/fetch/index.html"
+  },
+  {
+    "revision": "1d28717c613ffb7c0a95",
+    "url": "/fetch/static/css/main.d7dd107b.chunk.css"
+  },
+  {
+    "revision": "f342c2681e9f6d58ebf4",
+    "url": "/fetch/static/js/2.6564ee09.chunk.js"
+  },
+  {
+    "revision": "1d28717c613ffb7c0a95",
+    "url": "/fetch/static/js/main.a6f55ae2.chunk.js"
+  },
+  {
+    "revision": "9fdd11131088402c0392",
+    "url": "/fetch/static/js/runtime-main.61a7393a.js"
+  }
+]);
